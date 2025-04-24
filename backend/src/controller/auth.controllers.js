4bb313f2c0f56dcb83e8e9857c4c38c15ee6d071
Mohanvasu <mohanmgs232101@gmail.com>
2025-04-24 23:23:50 +0530

    Added login endpoint

--- backend/src/controller/auth.controllers.js
+++ backend/src/controller/auth.controllers.js
@@ -61,7 +61,48 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    
+    const {email,password} = req.body;
+    try {
+        const user = await db.user.findUnique({
+            where : {
+                email
+            }
+        })
+        if(!user){
+            res.status(404).json({
+                success : false,
+                message : "User not found"
+            })
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            res.status(401).json({
+                success : false,
+                message : "Invalid credentials"
+            })
+        }
+
+        const token = jwt.sign({id: user.id},process.env.JWT_SECRET_KEY,{expiresIn : "7d"});
+
+        res.cookie("jwt",token,{
+            httpOnly : true,
+            sameSite : "strict",
+            secure : process.env.NODE_ENV !== "development",
+            maxAge : 1000 * 60 * 60 * 24 * 7  //7 days
+        })
+
+        res.status(200).json({
+            success : true,
+            message : "User logged in successfully"
+        })
+    } catch (error) {
+        console.log("Error occurred while login",error);
+        res.status(500).json({
+            success : false,
+            message : "Error occurred while login"
+        });
+    }
 }
 
 const logout = async (req, res) => {
